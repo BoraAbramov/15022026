c6099ead9defcd958afd8f80aad06a55447a6a4f
BoraAbramov <borya812@gmail.com>
2026-02-16 21:12:55 +0200

    update

--- task 2.py
+++ task 2.py
@@ -2,14 +2,23 @@
 votes = []
 
 while True:
-        votes.append(input("Please enter your ID: "))
-        votes[-1] = int(votes[-1])
-        if votes[-1] == -999:
-            votes.pop()
-            break
-        if votes[-1] < 0 or votes[-1] > 100:
-            votes.pop()
+    votes.append(input("Please enter your ID: "))
+    votes[-1] = int(votes[-1])
+    if votes[-1] == -999:
+        votes.pop()
+        break
+    if votes[-1] < 0 or votes[-1] > 100:
+        votes.pop()
 
 print(votes)
 
-votes = set(votes)
+votes_copy = votes.copy()
+votes_set = {}
+invalid_voters = {}
+_attemps = len(votes)
+
+for item in votes:
+    if votes.count(item) > 1:
+        invalid_voters.add(item)
+        votes.pop(item)
+

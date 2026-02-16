e825f282a6c41d36b11b0d81a3836aeb9c429bea
BoraAbramov <borya812@gmail.com>
2026-02-16 20:54:05 +0200

    update

--- task 2.py
+++ task 2.py
@@ -1,1 +1,15 @@
 
+votes = []
+
+while True:
+        votes.append(input("Please enter your ID: "))
+        votes[-1] = int(votes[-1])
+        if votes[-1] == -999:
+            votes.pop()
+            break
+        if votes[-1] < 0 or votes[-1] > 100:
+            votes.pop()
+
+print(votes)
+
+votes = set(votes)
